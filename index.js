eba929988bdf578f5f6ad2f5d9bdcfe9985860b0
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-09-20 09:20:47 -0600

    commit tree 5c11dc502394b4ed239c607879aaa98319b31df5

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName
+}
+
+var bestCustomer
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return;
+}
+
+const leastFavoriteCustomer = "maria";
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "gary"
+}
